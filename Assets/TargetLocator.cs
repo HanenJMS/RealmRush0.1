45f5494eb0cf59871c3abb1dbea7f39eef95b1cc
HanenJMS <Hanensgamelab@gmail.com>
2022-03-24 23:18:48 -0400

    RealmRush.Section125.TargetClosestEnemy

--- Assets/TargetLocator.cs
+++ Assets/TargetLocator.cs
@@ -5,36 +5,62 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField] ParticleSystem projectileSystem;
+    [SerializeField] float range = 15f;
     [SerializeField] Transform currentTarget;
 
     private void Start()
     {
-        foreach(Transform t in GetComponentInChildren<Transform>())
+        AttachWeapon();
+    }
+    private void Update()
+    {
+        FindClosestTarget();
+        AimWeapon();
+    }
+    private void AttachWeapon()
+    {
+        foreach (Transform t in GetComponentInChildren<Transform>())
         {
-            if(t.name.Equals("BallistaTopMesh"))
+            if (t.name.Equals("BallistaTopMesh"))
             {
                 weapon = t;
             }
         }
+        //projectileSystem = GetComponentInChildren<ParticleSystem>();
     }
-
-    private void Update()
+    void FindClosestTarget()
     {
-        AimWeapon();
-        if(currentTarget == null)
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+        foreach(Enemy enemy in enemies)
         {
-            foreach(EnemyHealth enemy in FindObjectsOfType<EnemyHealth>())
+            float targetDistance = Vector3.Distance(this.transform.position, enemy.transform.position);
+            if (targetDistance < maxDistance)
             {
-                if(enemy.isAlive())
-                {
-                    currentTarget = enemy.gameObject.transform;
-                    AimWeapon();
-                }
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
             }
         }
+        currentTarget = closestTarget;
     }
     void AimWeapon()
     {
+        float targetDistance = Vector3.Distance(transform.position, currentTarget.position);
         weapon.LookAt(currentTarget);
+        if(targetDistance < range)
+        {
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
+    }
+    void Attack(bool isActive)
+    {
+        var emissionModule = projectileSystem.emission;
+        emissionModule.enabled = isActive;
     }
 }
